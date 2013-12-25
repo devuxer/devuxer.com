3159352bebe6aa0017821107dfae319184bc6f38
Dan M <danspamable@gmail.com>
2013-12-24 21:00:05 -0800

    No longer tracking Snow
    Moved Snow to separate project.

--- compile.snow.bat
+++
@@ -1,3 +0,0 @@
-"D:\Projects\Open Source\Sandra.Snow\src\Snow\bin\Release\Snow.exe" config="D:\Projects\Open Source\devuxer.com\Snow\snow"
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" localhost:12116
-"C:\Program Files\IIS Express\iisexpress.exe" -site:Devuxer
