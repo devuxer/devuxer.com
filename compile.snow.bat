765fe815ef55647cb48fd6768407a012848457ec
Dan M <danspamable@gmail.com>
2013-12-24 18:15:42 -0800

    Enhanced shell script and relocated images
    Images were in the wrong location

--- compile.snow.bat
+++ compile.snow.bat
@@ -1,1 +1,3 @@
-.\Snow\_compiler\Snow.exe config=.\Snow\
+"D:\Projects\Open Source\Sandra.Snow\src\Snow\bin\Release\Snow.exe" config="D:\Projects\Open Source\devuxer.com\Snow\snow"
+"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" localhost:12116
+"C:\Program Files\IIS Express\iisexpress.exe" -site:Devuxer
